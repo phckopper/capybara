ade56fc227f6f6af24bc01bb38e0d743d28a3089
Pedro Henrique Kopper <pedrohkopper@gmail.com>
2023-03-05 19:45:22 -0300

    Add websocket interface

--- main.py
+++ main.py
@@ -1,22 +1,52 @@
 #!/usr/bin/python
 
-import time
+import asyncio
+from http import HTTPStatus
+import json
+from websockets.http import Headers
+import websockets
 from lib.MotorDriver import MotorDriver
 
-
-print("this is a motor driver test code")
 motor = MotorDriver()
 
-print("forward 2 s")
-motor.run(0, 'forward', 100)
-motor.run(1, 'forward', 100)
-time.sleep(2)
+def vector_to_commands(vec):
+    commands = [100 * vec[0], 100 * vec[0]]
+    commands[0] += 100 * vec[1]
+    commands[1] -= 100 * vec[1]
+    if commands[0] > 100 or commands[1] > 100 :
+        commands[0] *= 100/max(commands)
+        commands[1] *= 100/max(commands)
+    if commands[0] < -100 or commands[1] < -100 :
+        commands[0] *= 100/max(commands)
+        commands[1] *= 100/max(commands)
+    return commands
 
-print("backward 2 s")
-motor.run(0, 'backward', 100)
-motor.run(1, 'backward', 100)
-time.sleep(2)
+async def http_handler(path, headers):
+    """Route HTTP requests to their handlers"""
 
-print("stop")
-motor.stop(0)
-motor.stop(1)
+    if path == '/':
+        with open("index.html") as f:
+            headers = Headers(**{'Content-Type': 'text/html'})
+            body = bytes(f.read(), 'utf-8')
+
+            return HTTPStatus.OK, headers, body
+
+    else:
+        return None
+
+async def handler(websocket):
+    while True:
+        message = await websocket.recv()
+        packet = json.loads(message)
+        (command_left, command_right) = vector_to_commands((packet["throttle"], packet["direction"]))
+        motor.run(0, 'forward' if command_left > 0 else 'backward', abs(int(command_left)))
+        motor.run(1, 'forward' if command_right > 0 else 'backward', abs(int(command_right)))
+
+
+async def main():
+    async with websockets.serve(handler, "", 8001, process_request=http_handler):
+        await asyncio.Future()  # run forever
+
+
+if __name__ == "__main__":
+    asyncio.run(main())
